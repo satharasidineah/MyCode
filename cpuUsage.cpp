08acf85ea4dc0e6a6d44c0565bcacd5082d1cb2f
satharasidinesh <satharasidinesh@gmail.com>
2023-01-18 10:28:11 +0530

    modified logic

--- cpuUsage.cpp
+++ cpuUsage.cpp
@@ -1,59 +1,28 @@
-#import <mach/mach.h>
-
-float getCpuUsage() {
-    kern_return_t kr;
-    task_info_data_t tinfo;
-    mach_msg_type_number_t task_info_count;
-
-    task_info_count = TASK_INFO_MAX;
-    kr = task_info(mach_task_self(), TASK_BASIC_INFO, (task_info_t)tinfo, &task_info_count);
-    if (kr != KERN_SUCCESS) {
-        return -1;
-    }
-
-    task_basic_info_t basic_info;
-    thread_array_t thread_list;
-    mach_msg_type_number_t thread_count;
-
-    thread_info_data_t thinfo;
-    mach_msg_type_number_t thread_info_count;
-
-    thread_basic_info_t basic_info_th;
-    uint32_t stat_thread = 0;
-
-    basic_info = (task_basic_info_t)tinfo;
-
-    kr = task_threads(mach_task_self(), &thread_list, &thread_count);
-    if (kr != KERN_SUCCESS) {
-        return -1;
-    }
-    if (thread_count > 0)
-        stat_thread += thread_count;
-
-    long tot_sec = 0;
-    long tot_usec = 0;
-    float tot_cpu = 0;
-    int j;
-
-    for (j = 0; j < thread_count; j++) {
-        thread_info_count = THREAD_INFO_MAX;
-        kr = thread_info(thread_list[j], THREAD_BASIC_INFO,
-                         (thread_info_t)thinfo, &thread_info_count);
-        if (kr != KERN_SUCCESS) {
-            return -1;
-        }
-
-        basic_info_th = (thread_basic_info_t)thinfo;
-
-        if (!(basic_info_th->flags & TH_FLAGS_IDLE)) {
-            tot_sec = tot_sec + basic_info_th->user_time.seconds + basic_info_th->system_time.seconds;
-            tot_usec = tot_usec + basic_info_th->user_time.microseconds + basic_info_th->system_time.microseconds;
-            tot_cpu = tot_cpu + basic_info_th->cpu_usage / (float)TH_USAGE_SCALE;
-        }
-    }
-
-    kr = vm_deallocate(mach_task_self(), (vm_offset_t)thread_list, thread_count * sizeof(thread_t));
-    assert(kr == KERN_SUCCESS);
-
-    return tot_cpu;
+void cpuUsage()
+{
+	unsigned numCPUs;
+	float totalCpuUsage;
+	processor_info_array_t cpuInfo;
+	mach_msg_type_number_t numCpuInfo;
+    int mib[2U] = { CTL_HW, HW_NCPU };
+    size_t sizeOfNumCPUs = sizeof(numCPUs);
+    int status = sysctl(mib, 2U, &numCPUs, &sizeOfNumCPUs, NULL, 0U);
+    if(status)
+        numCPUs = 1;
+	std::cout<<"numCPUs:"<<numCPUs<<std::endl;
+	CPUUsageLock = [[NSLock alloc] init];
+	[CPUUsageLock lock];
+	for(unsigned i = 0U; i < numCPUs; ++i) {
+		float inUse, total;
+		inUse = cpuInfo[(CPU_STATE_MAX * i) + CPU_STATE_USER] + cpuInfo[(CPU_STATE_MAX * i) + CPU_STATE_SYSTEM] + cpuInfo[(CPU_STATE_MAX * i) + CPU_STATE_NICE];
+        total = inUse + cpuInfo[(CPU_STATE_MAX * i) + CPU_STATE_IDLE];
+		std::cout<<"inUse:"<<inUse<<std::endl;
+		std::cout<<"total:"<<total<<std::endl;
+		float usagePercentage = inUse / total
+		std::cout<<"usagePercentage:"<<usagePercentage<<std::endl;
+		totalCpuUsage += usagePercentage;
+	}
+	usagePer = totalCpuUsage/numCPUs;
+	std::cout<<"usagePer:"<<usagePer<<std::endl;
+	[CPUUsageLock unlock];
 }
